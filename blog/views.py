a319f5651253e8dedecedd69d82e86cf6986d030
reshoketswe matloga <trmatloga@gmail.com>
2020-02-20 23:21:31 +0200

    Home Page And base template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from urllib import request
 from django.http import HttpResponse
+from django.template import Context
 
+
+generic = [{'author':'Reshoketswe','content':'Sample Content', 'date':'20-20-2020' , 'title':'sample tile'}]
 
 def home(request):
-	return HttpResponse("<h1> Hello world</h1>")
+	context = {'key_1':generic}
+	return render(request, 'blog/home.html',context = context)
