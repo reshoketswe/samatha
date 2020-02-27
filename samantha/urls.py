85c9264ca2117406b7643d139ba7da69ea33a2c1
reshoketswe matloga <trmatloga@gmail.com>
2020-02-27 20:55:02 +0200

    Login and Logout

--- samantha/urls.py
+++ samantha/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from blog import views
 
 urlpatterns = [
+	url('login/',auth_views.LoginView.as_view(template_name = 'blog/login.html'), name = 'login'),
+	url('logout/',auth_views.LogoutView.as_view(template_name = 'blog/logout.html'), name = 'logout'),
     url(r'^admin/', admin.site.urls),
     url('home/',views.home , name = 'home'),
     url('register/',views.register , name = 'register'),
